61815925c52cf7a863d39fce68d263a084164125
Ron Caulin <wilcaulin2010@yahoo.com>
2014-11-25 14:06:46 +0800

    SampleResponsiveMenu
    responsive menu

---
+++ js/SlideUpMenu.js
@@ -0,0 +1,32 @@
+﻿
+/*
+        SlideUpMenu
+        Created by: Wilando Ron Caulin
+
+*/
+
+(function ($) {
+    $(document).ready(function () {
+
+        $.fn.SlideUpMenu = function (options) {
+            var settings = $.extend({ dataheight: 100 }, options);
+
+            var $this = $(this);
+            $this.css("margin-top", "-" + settings.dataheight + "px");
+            $this.css("cursor","pointer");
+
+            $this.mouseenter(function () {
+                $this.animate({
+                    "margin-top": "0"
+                });
+            });
+
+            $this.mouseleave(function () {
+                $this.animate({
+                    "margin-top": "-" + settings.dataheight + "px"
+                });
+            });
+        };
+
+    });
+} (jQuery));
---
+++ readme.txt
@@ -0,0 +1,2 @@
+All right reserved 2014
+&copy; 2014 WRC
